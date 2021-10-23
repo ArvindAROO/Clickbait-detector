60a416375fd58527c376f317aac447e90745339c
JeevanSamrudh <jeevansamrudh0707@gmail.com>
2021-10-24 00:52:40 +0530

    Added plugin folder for 3rd time

--- plugin/background.js
+++ plugin/background.js
@@ -1,5 +1,12 @@
+const fs = require('fs')
+  
+
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
     // use `url` here inside the callback because it's asynchronous!
-    alert(url);
+    
+    fs.writeFile('C:/Users/91807/Desktop/Project/Hallothon_2021/self/Output.txt', url, (err) => {
+        if (err) alert("err");
+        alert("success")
+    })
 });
