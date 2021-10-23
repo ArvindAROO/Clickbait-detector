d08a080a145e1230bff119a8f7797c5f15e4f3ba
JeevanSamrudh <jeevansamrudh0707@gmail.com>
2021-10-24 00:32:08 +0530

    Added plugin folder for 2nd time

--- plugin/background.js
+++ plugin/background.js
@@ -1,5 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    var url = window.location.href;
-    console.log(url);
-  });
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+    alert(url);
+});
--- plugin/url.js
+++
@@ -1,2 +0,0 @@
-var url = window.location.href;
-console.log(url);
