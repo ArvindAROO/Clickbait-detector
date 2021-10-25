e0e4859ead68089774ce249d912eb847965156b5
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2021-10-25 16:53:13 +0530

    Delete python directory

--- python/client.py
+++
@@ -1,8 +0,0 @@
-from newspaper2 import getSummary
-
-import percentage
-def main():
-    title, keywords, articleSummary, nlpSummary = getSummary("https://www.bbc.co.uk/sport/formula1/59017996")
-    percentage.findPertentage(title, keywords, articleSummary, nlpSummary)
-if __name__ == '__main__':
-    main()
